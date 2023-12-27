5be690c3de53d0f30661c369825454a5205b7dc4
gentaarya <mgentaarya@gmail.com>
2023-12-27 19:13:54 +0700

    create and fix layouting feature

--- src/Test/View/User/Main.jsx
+++ src/Test/View/User/Main.jsx
@@ -21,19 +21,18 @@ const Main = () => {
 , Service AC Terpercaya Tangkas Jaya Teknik , AC Service Tangkas  Jaya Teknik"
         />
         <meta name="author" content="wayan" />
-        {/* Open Graph meta tags */}
+
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={pageDescription} />
         <meta property="og:image" content={imageUrl} />
         <meta property="og:url" content={pageUrl} />
         <meta property="og:type" content="website" />
-        {/* Twitter Card meta tags */}
+
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@YourTwitterHandle" />
         <meta name="twitter:title" content={pageTitle} />
         <meta name="twitter:description" content={pageDescription} />
         <meta name="twitter:image" content={imageUrl} />
-        {/* Add other meta tags, styles, scripts, etc. here */}
       </Helmet>
       <IndexHome />
     </div>
